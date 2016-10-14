ac733cfad9a240358d4e25253b1965540fa32cba
Sam Oehert <sam.oehlert@gmail.com>
2016-10-14 00:44:40 -0500

    add error checking

--- dynamic-hostsfile-inventory.py
+++ dynamic-hostsfile-inventory.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
 import os
 import sys
+import signal
 import argparse
 
+from IPy import IP
 from configparser import ConfigParser
+
+def die():
+    """ Function to kill program on error """
+    sys.exit(0)
+
+def test_bool(bool_var):
+    """ Test if a boolean is true and exists """
+    try:
+        bool_var
+    except NameError:
+        print("Failure with bool_var")
+    else:
+        return True
+
+def test_file(file_name):
+    """ Test to see if file exists/can be accessed """
+    if os.path.isfile(file_name):
+        return True
+    else:
+        print("\nERROR: Cannot access {}\nPossible Issues:\n\tDoes it exist?\n\tDo you have permissions to read it?\n".format(file_name))
+        die()
 
 def local_connection():
     """ Set ssh values all to nothing since we don't need them """
@@ -36,7 +59,11 @@ if conf_file:
         server, port, user, ssh_key = local_connection()
     else:
         server = conf.get('ssh', 'server')
-        port = conf.get('ssh', 'port')
+        try:
+            port = conf.getint('ssh', 'port')
+        except ValueError:
+            print("\nERROR: Conf file argument 'port': invalid int value: '{}'\n".format(conf.get('ssh', 'port')))
+            die()
         user = conf.get('ssh', 'user')
         ssh_key = conf.get('ssh', 'ssh_key')
         local_conn = False
@@ -57,8 +84,7 @@ conf_parser = argparse.ArgumentParser(parents=[parser], description=__doc__)
 
 conf_parser.add_argument("-f", "--file", help="Path of hosts file", dest='hosts_file')
 conf_parser.add_argument("-i", "--inventory", help="Path of inventory file", dest='inventory')
-conf_parser.add_argument("-s", "--server", help="Server to connect to (ip address)", dest='server')
-conf_parser.add_argument("-p", "--port", help="SSH port to connect over", dest='port')
+conf_parser.add_argument("-s", "--server", help="Server to connect to (ip address)", dest='server') conf_parser.add_argument("-p", "--port", help="SSH port to connect over", type=int, dest='port')
 conf_parser.add_argument("-u", "--user", help="User to connect as", dest='user')
 conf_parser.add_argument("-k", "--key", help="SSH key to use", dest='ssh_key')
 
@@ -69,13 +95,35 @@ for arg, value in vars(cli_args).items():
     if value:
         exec(arg + '=value')
 
+
 if __name__ == '__main__':
-    print("local: {}".format(local_conn))
-    print("password: {}".format(password_auth))
-    print("config: {}".format(conf_file))
-    print("hosts: {}".format(hosts_file))
-    print("inventory: {}".format(inventory))
-    print("server: {}".format(server))
-    print("port: {}".format(port))
-    print("user: {}".format(user))
-    print("ssh key: {}".format(ssh_key))
+    if test_bool(local_conn):
+        print("local: {}".format(local_conn))
+    if test_bool(password_auth):
+        print("password: {}".format(password_auth))
+
+    print("conf: {}".format(conf_file))
+
+    if test_file(hosts_file):
+        print("hosts: {}".format(hosts_file))
+    if test_file(inventory):
+        print("inventory: {}".format(inventory))
+
+    if not local_conn:
+        print("user: {:s}".format(user))
+        if test_file(ssh_key):
+            print("ssh key: {}".format(ssh_key))
+
+        try:
+            print("port: {:d}".format(port))
+        except TypeError:
+            print("Port should be an integer")
+            die()
+
+        try:
+            print("server: {}".format(IP(server)))
+        except:
+            if server:
+                print("\nERROR: Not a valid IP address\n")
+                die()
+
